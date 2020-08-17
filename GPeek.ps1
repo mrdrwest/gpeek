52b60b01f88ec7b60a649328ce41a808eb2d9085
Darrick West <mrdrwest@hotmail.com>
2020-08-17 19:26:49 -0400

    added paramters-modified script logic

--- GPeek.ps1
+++ GPeek.ps1
@@ -8,52 +8,76 @@
     is stored in if there are many backups.
 
     GPeek.ps1 recursively searches thru the root GPO backup folder
-    for a GPO backup.xml file and creates a custom object that
+    for a GPO $BACKUPXML file and creates a custom object that
     represents a backed up GPO path, ID, and display name, and 
     writes what it finds to the console. GPeek.json file defines
     the root backup path (modify as required). GPeek.json must be in
     the same folder as GPeek.ps1.
 #>
 
-# Initialize array to store custom objects
-$gpoTable=@()
-$rootPathGPOBackup=(Get-Content .\GPeek.json | ConvertFrom-Json).rootPathGPO
+$BACKUPXML="backup.xml"
+function Get-BackupGPOName {
+    [CmdletBinding()]
+    param (
+        # Parameter help description
+        [Parameter(mandatory=$false)]
+        [string] 
+        $pathGPOBackup=""
+    )
 
-# Recursively search GPO backup path for backup.xml
-(Get-ChildItem $rootPathGPOBackup -Include backup.xml -Recurse) | ForEach-Object {
-    #Initialize custom object to store GPO information
-    $customObject = [PSCustomObject] @{
-        "GPOBackupPath" = ""
-        "GPOGuid"       = ""
-        "GPODisplayName" = ""
+    $ErrorView = "ConciseView"
+    # Assign GPO backup path via parameter or JSON configuruation file
+    Test-Path $pathGPOBackup -ErrorAction SilentlyContinue | Out-Null &&
+        ($pathGPOBackup=(Get-Content .\GPeek.json | ConvertFrom-Json).rootPathGPO) | Out-Null
+    
+    # Recursively search GPO backup path for $BACKUPXML
+    $backupXmlFilePath=Get-ChildItem $pathGPOBackup -Include $BACKUPXML -Recurse
+    #Get-ChildItem -LiteralPath $pathGPOBackup -Include $BACKUPXML -Recurse -OutVariable $backupXmlFilePath
+    
+    If (-not $backupXmlFilePath.Exists) {
+        "$BACKUPXML not found : $([char]27)[38;2;255;0;0m ☹"
+        Exit
     }
+    
+    # Initialize array to store custom objects
+    $gpoTable=@()
 
-    # Parse GPO backup.xml file using XML methods
-    $xml=[xml](Get-Content $PSItem)
-    $gpoBackupPath=$PSItem.Directory.FullName
-    $gpoGuid= $xml.GroupPolicyBackupScheme.GroupPolicyObject.GroupPolicyCoreSettings.ID.'#cdata-section'
-    $gpoDisplayName=$xml.GroupPolicyBackupScheme.GroupPolicyObject.GroupPolicyCoreSettings.DisplayName.'#cdata-section'
+    $backupXmlFilePath | ForEach-Object {
+        
+        #Initialize custom object to store GPO information
+        $customObject = [PSCustomObject] @{
+            "GPOBackupPath" = ""
+            "GPOGuid"       = ""
+            "GPODisplayName" = ""
+        }
+            
+        # Parse GPO $BACKUPXML file using XML methods
+        $xml=[xml](Get-Content $PSItem)
+        $gpoBackupPath=$PSItem.Directory.FullName
+        $gpoGuid= $xml.GroupPolicyBackupScheme.GroupPolicyObject.GroupPolicyCoreSettings.ID.'#cdata-section'
+        $gpoDisplayName=$xml.GroupPolicyBackupScheme.GroupPolicyObject.GroupPolicyCoreSettings.DisplayName.'#cdata-section'
+        
+        # Add GPO backup information to custom object
+        $customObject.GPOBackupPath=$gpoBackupPath
+        $customObject.GPOGuid=$gpoGuid
+        $customObject.GPODisplayName=$gpoDisplayName
 
-    # Add GPO backup information to custom object
-    $customObject.GPOBackupPath=$gpoBackupPath
-    $customObject.GPOGuid=$gpoGuid
-    $customObject.GPODisplayName=$gpoDisplayName
-
-    # Update index into custom object for next GPO information
-    $gpoTable+=$customObject
-
-    # Add NoteProperty to custom object to reference entries by index number
-    $idx=0
-    $gpoTable | ForEach-Object {
-        Add-Member `
+        # Update index into custom object for next GPO information
+        $gpoTable+=$customObject
+        
+        # Add NoteProperty to custom object to reference entries by index number
+        $idx=0
+        $gpoTable | ForEach-Object {
+            Add-Member `
             -InputObject $PSItem `
             -Name Index `
             -Value $idx `
             -MemberType NoteProperty `
             -Force
-    $idx++    
-    }
-}    
-Write-Output $gpoTable | Select-Object Index, GPOBackupPath, GPOGuid, GPODisplayName
-$gpoTable=$null
-$customObject=$null
+            $idx++    
+        }
+    }    
+    Write-Output $gpoTable | Select-Object Index, GPOBackupPath, GPOGuid, GPODisplayName
+    $gpoTable=$null
+    $customObject=$null
+}
